7b68a75580cb64e286412778358cac8dd66ed715
Devansh Sarup Mathur <52082688+devanshsmathur@users.noreply.github.com>
2025-05-03 10:17:45 +1000

    Time: 0 ms (100%), Space: 45.5 MB (43.34%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int missing = 0;
+        
 
         int expsum = n*(n+1)/2;
 
@@ -9,7 +9,7 @@ class Solution {
         for (int i=0;i<n;i++){
             actsum+=nums[i];
         }
-        missing = expsum-actsum;
+        
 
-    return missing;
+    return expsum-actsum;
 }}
