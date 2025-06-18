31994211b3615ca71c841af0fbcb1988f3880227
Devansh Sarup Mathur <52082688+devanshsmathur@users.noreply.github.com>
2025-06-18 10:28:10 +1000

    Time: 2 ms (96.92%), Space: 133.5 MB (86.04%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
         if(nums==null || nums.length<3) return false;
-        int a=(~0) >>> 1;
-        int b=(~0) >>> 1;
+        int a=Integer.MAX_VALUE;
+        int b=Integer.MAX_VALUE;
 
         for(int i=0; i<nums.length;i++){
             if(a>=nums[i]){
