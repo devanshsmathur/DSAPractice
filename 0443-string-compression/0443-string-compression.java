021df44a693fc27e02615db7a574f7f9bd72add9
Devansh Sarup Mathur <52082688+devanshsmathur@users.noreply.github.com>
2025-06-29 13:23:07 +1000

    Time: 1 ms (99.49%), Space: 44.1 MB (67.16%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,25 +1,25 @@
 class Solution {
     public int compress(char[] chars) {
-        int ans=0;
         int i=0;
+        int index=0;
+        
         while(i<chars.length){
-            char letter = chars[i];
+            int letter = chars[i];
             int count=0;
+            chars[index++]=chars[i];
+
 
             while(i<chars.length && chars[i]==letter){
-                ++count;
-                ++i;
+                count++;
+                i++;
             }
-            chars[ans++] = letter;
 
             if(count>1){
-                // to convert "12" to "1","2"
-                for(char c: String.valueOf(count).toCharArray()){
-                    chars[ans++]=c;
+                for(Character c:String.valueOf(count).toCharArray()){
+                    chars[index++]=c;
                 }
             }
         }
-
-    return ans; // we return only the length of "compressed" instead of length of array
+        return index;
     }
 }
